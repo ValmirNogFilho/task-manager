1e62bde744f00e471bcd01b17dd7e24d477243cc
Valmir Filho <valmirnogueira514@gmail.com>
2025-05-11 02:58:01 -0300

    :sparkles: feat: filtering status and priority fields

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
   has_rich_text :description
   validates :title, presence: true
+  enum :status, { to_do: 0, in_progress: 1, done: 2 }
+  enum :priority, { low: 0, medium: 1, high: 2 }
 end
