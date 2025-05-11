f4f989675d299ac02a6a781fda12034881eb097f
Valmir Filho <valmirnogueira514@gmail.com>
2025-05-11 16:57:53 -0300

    build: change db to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_05_11_060734) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -55,7 +58,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_11_060734) do
     t.integer "priority"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
